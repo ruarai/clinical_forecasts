f21003cb746aecd95acc19def69f24f58ace971c
Ruarai Tobin <ruarai.tobin@unimelb.edu.au>
2022-08-22 05:43:55 +0000

    _targets.R changes

--- _targets.R
+++ _targets.R
@@ -1,5 +1,6 @@
 library(targets)
 library(tarchetypes)
+library(future.callr)
 
 options(tidyverse.quiet = TRUE)
 tar_option_set(packages = c("tidyverse", "lubridate", "curvemush"), garbage_collection = TRUE, backoff = 60)
@@ -11,27 +12,30 @@ state_tbl <- tibble::tibble(state_modelled = c("VIC", "ACT", "QLD", "NSW", "NT",
 longterm <- FALSE
 
 t_parameters <- list(
-  tar_target(date_forecasting, ymd("2022-07-08")),
+  tar_target(date_forecasting, ymd("2022-08-17")),
   
   
-  tar_target(date_simulation_start, ymd("2021-11-01")),
-  tar_target(forecast_name, str_c("fc_", date_forecasting, "_final")),
+  tar_target(date_simulation_start, ymd("2022-04-01")),
+  tar_target(forecast_name, str_c("fc_", date_forecasting, "_test_smc")),
   
   # Update these to the latest file path
   # ~/mfluxshared and ~/mfluxunimelb should point to the (respective) mediaflux server
-  tar_target(raw_nindss, "~/mfluxshared/Health Uploads/COVID-19 UoM 30Jun2022.zip"),
-  tar_target(raw_local_cases, "~/mfluxunimelb/local_cases_input/local_cases_input_2022-06-28.csv"),
-  tar_target(raw_ensemble, "~/mfluxshared/forecast-outputs/combined_samples_75asc2022-06-14.csv"),
+  tar_target(raw_nindss, "~/mfluxshared/Health Uploads/COVID-19 UoM 16Aug2022.zip"),
+  tar_target(raw_local_cases, "~/mfluxunimelb/local_cases_input/local_cases_input_2022-08-16.csv"),
+  tar_target(raw_ensemble, "~/mfluxshared/forecast-outputs/combined_samples_75asc2022-08-09.csv"),
   
-  #tar_target(models_included, c("gar", "moss", "dst")),
-  tar_target(models_included, c("gar", "moss")),
+  tar_target(models_included, c("gar", "moss", "dst")),
   
-  tar_target(nindss_bad_states, c("NT", "SA", "QLD", "TAS", "VIC")),
+  tar_target(nindss_bad_states, c("NT", "SA", "QLD")),
+  
+  tar_target(do_upload_trajectories, FALSE),
+  
+  
+  
+  
+  
   tar_target(use_fitting, TRUE),
-  
-  tar_target(is_longterm, longterm),
-  
-  tar_target(do_upload_trajectories, FALSE)
+  tar_target(is_longterm, longterm)
 )
 
 
